278b0a07c01236e6e8939a13c655977693c2d2b4
Gerson <3005563490101@ingenieria.usac.edu.gt>
2022-10-18 23:03:37 -0600

    insert

--- backend/interprete/instrucciones/FuncionesVector.py
+++ backend/interprete/instrucciones/FuncionesVector.py
@@ -1,3 +1,4 @@
+from msilib.schema import SelfReg
 from ..expresiones.Expresion import Expresion
 from ..extra.Tipos import TipoDato
 from ..extra.Simbolo import Simbolo
@@ -36,6 +37,7 @@ class Push(Instruccion):
             STACK[pos] = tempRetorno;
         '''
         # ejecutando la expresión
+        self.generador.addComentario('PUSH');
         self.expresion.generador = self.generador;
         val:RetornoExpresion = self.expresion.ejecutar(console, scope);
         # obtenemos el vector
@@ -83,48 +85,59 @@ class Push(Instruccion):
         self.generador.setStack(pos, tempRetorno);
 
 class Insert(Instruccion):
-    def __init__(self, id:str, exp1, exp2, linea: int, columna: int):
+    def __init__(self, id:Expresion, exp1:Expresion, exp2:Expresion, linea: int, columna: int):
         super().__init__(linea, columna);
         self.id = id;
         self.exp1 = exp1;
         self.exp2 = exp2;
     
     def ejecutar(self, console: Console, scope: Scope):
+        '''
+        <código de valPos>
+        <código de valExp>
+        <código de vector>
+        tempPos = vector.valor + valPos.valor;
+        HEAP[tempPos] = valExp.valor;
+        '''
         # ejecutando las expresiones
-        val1 = self.exp1.ejecutar(console, scope);
-        val2 = self.exp2.ejecutar(console, scope);
+        self.generador.addComentario('INSERT');
+        self.exp1.generador = self.generador;
+        valPos:RetornoExpresion = self.exp1.ejecutar(console, scope);
+        self.exp2.generador = self.generador;
+        valExp:RetornoExpresion = self.exp2.ejecutar(console, scope);
         # obtenemos el vector
+        self.id.generador = self.generador;
         vector:Simbolo = self.id.ejecutar(console, scope);
-        if (vector.esVector == None):
+        if (vector.atrArr.esVector == None):
             # ERROR. No es un vector
             _error = _Error(f'La variable {vector.id} no es un vector, no contiene la función Insert', scope.ambito, self.linea, self.columna, datetime.now());
             raise Exception(_error);
-        if (not vector.esVector):
-            # ERROR. Los arreglos no contiene la función insert
-            _error = _Error(f'Los arreglos {vector.id} no contiene la función insert', scope.ambito, self.linea, self.columna, datetime.now());
-            raise Exception(_error);
-        if (val2.tipo != vector.tipo):
+        if (valExp.tipo != vector.tipo):
             # ERROR. Tipos incompatibles
-            _error = _Error(f'Tipos incompatibles. No se puede almacenar una expresión {val2.tipo.name} en una variable de tipo {vector.tipo.name}', scope.ambito, self.linea, self.columna, datetime.now());
+            _error = _Error(f'Tipos incompatibles. No se puede almacenar una expresión {valExp.tipo.name} en una variable de tipo {vector.tipo.name}', scope.ambito, self.linea, self.columna, datetime.now());
             raise Exception(_error);
-        if (val1.tipo != TipoDato.INT64):
+        if (valPos.tipo != TipoDato.INT64):
             # ERROR. No se puede acceder al indice val1.tipo.
-            _error = _Error(f'No se puede acceder al indice {val1.tipo.name}', scope.ambito, self.linea, self.columna, datetime.now());
+            _error = _Error(f'No se puede acceder al indice {valPos.tipo.name}', scope.ambito, self.linea, self.columna, datetime.now());
             raise Exception(_error);
-        vector.valor.insert(val1.valor, val2.valor);
-        # ahora revisaremos si se trata de un vector con un tamaño definido
-        if (vector.with_capacity != None):
-            if (vector.with_capacity < len(vector.valor)):
-                vector.with_capacity = vector.with_capacity * 2;
+        tempPos:str = self.generador.newTemp();
+        self.generador.addOperacion(tempPos, vector.valor, valPos.valor, '+');
+        self.generador.setHeap(tempPos, valExp.valor);
         scope.setValor(self.id.id, vector, self.linea, self.columna);
 
 class Remove(Instruccion):
-    def __init__(self, id:str, exp, linea: int, columna: int):
+    def __init__(self, id:Expresion, exp:Expresion, linea: int, columna: int):
         super().__init__(linea, columna);
         self.id = id;
         self.exp = exp;
 
     def ejecutar(self, console: Console, scope: Scope):
+        '''
+        <código de valPos>
+        <código de vector>
+        tempPos = vector.valor + valPos.valor;
+        tempDelete = HEAP[tempPos];
+        '''
         # indice del elemento a eliminar
         val = self.exp.ejecutar(console, scope);
         # obtenemos el vector
@@ -211,6 +224,7 @@ class Capacity(Instruccion):
         temp = vector.with_capacity;
         '''
         # obtenemos el vector
+        self.generador.addComentario('CAPACITY');
         self.id.generador = self.generador;
         vector:RetornoExpresion = self.id.ejecutar(console, scope);
         if (vector.atrArr.esVector == None):
